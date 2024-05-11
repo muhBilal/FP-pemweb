f2ee7bc3d52535290ceb31ed25951aa6e903e229
muhBilal <mb336450@gmail.com>
2024-05-11 16:41:00 +0700

    :sparkles: feat: add home layouts

---
+++ pages/home.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <?php include '../templates/navbar.php'; ?>
+
+
+    <section class="bg-[#1A2035] h-screen p-5 sm:ml-72">
+        <div class="bg-gray-100 h-full rounded-xl overflow-auto">
+            <div class="h-full">
+                <div class="pl-5 pt-14 sm:pl-24 pr-5 pb-10 mx-auto h-full;">
+                    <section class="flex flex-col md:flex-row justify-between items-start md:items-center gap-3">
+                        <h1 class="text-start text-3xl font-semibold">Beranda</h1>
+                    </section>
+                    <section class="flex mx-auto flex-col md:flex-row gap-5 mt-0 md:mt-5">
+                        <div class="w-full md:w-1/3 flex flex-col gap-2 mb-10">
+                            <div class="group">
+                                <div class="mt-10 z-30 bg-white h-[270px] relative p-5 rounded-xl shadow-sm w-full group-hover:h-[310px] group-hover:shadow-lg duration-500 ease-in-out">
+                                    <img src="/images/animal.webp" alt="animal" class="h-[70%] w-[90%] object-cover rounded-xl absolute top-5 left-1/2 -translate-x-1/2 group group-hover:-translate-y-1/2 group-hover:w-[250px] group-hover:h-[140px] duration-500 ease-in-out transform-gpu" />
+                                    <div class="group text-center w-full px-5 absolute bottom-5 left-1/2 -translate-x-1/2 group-hover:bottom-10 duration-500 ease-in-out transform-gpu">
+                                        <h1 class="group text-center mt-2 mb-2 font-semibold text-lg text-primary  duration-1000 ease-in-out">
+                                            Hewan
+                                        </h1>
+                                        <p class="group hidden text-sm mb-3 group-hover:block  duration-1000 ease-in-out opacity-0 group-hover:opacity-100">
+                                            Jelajahi beragam hewan dari seluruh provinsi. Dari mamalia
+                                            eksotis hingga reptil yang misterius, yang telah kami
+                                            kumpulkan untuk kaamu nikmati
+                                        </p>
+                                        <Link href="/animals" class="group mx-auto hidden btn-primary-lite text-sm group-hover:block  duration-1000 ease-in-out opacity-0 group-hover:opacity-100">
+                                        Selengkapnya
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="group">
+                                <div class="mt-10 z-30 bg-white h-[270px] relative p-5 rounded-xl shadow-sm w-full group-hover:h-[310px] group-hover:shadow-lg duration-500 ease-in-out">
+                                    <img src="/images/animal.webp" alt="animal" class="h-[70%] w-[90%] object-cover rounded-xl absolute top-5 left-1/2 -translate-x-1/2 group group-hover:-translate-y-1/2 group-hover:w-[250px] group-hover:h-[140px] duration-500 ease-in-out transform-gpu" />
+                                    <div class="group text-center w-full px-5 absolute bottom-5 left-1/2 -translate-x-1/2 group-hover:bottom-10 duration-500 ease-in-out transform-gpu">
+                                        <h1 class="group text-center mt-2 mb-2 font-semibold text-lg text-primary  duration-1000 ease-in-out">
+                                            Hewan
+                                        </h1>
+                                        <p class="group hidden text-sm mb-3 group-hover:block  duration-1000 ease-in-out opacity-0 group-hover:opacity-100">
+                                            Jelajahi beragam hewan dari seluruh provinsi. Dari mamalia
+                                            eksotis hingga reptil yang misterius, yang telah kami
+                                            kumpulkan untuk kaamu nikmati
+                                        </p>
+                                        <Link href="/animals" class="group mx-auto hidden btn-primary-lite text-sm group-hover:block  duration-1000 ease-in-out opacity-0 group-hover:opacity-100">
+                                        Selengkapnya
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="group">
+                                <div class="mt-10 z-30 bg-white h-[270px] relative p-5 rounded-xl shadow-sm w-full group-hover:h-[310px] group-hover:shadow-lg duration-500 ease-in-out">
+                                    <img src="/images/animal.webp" alt="animal" class="h-[70%] w-[90%] object-cover rounded-xl absolute top-5 left-1/2 -translate-x-1/2 group group-hover:-translate-y-1/2 group-hover:w-[250px] group-hover:h-[140px] duration-500 ease-in-out transform-gpu" />
+                                    <div class="group text-center w-full px-5 absolute bottom-5 left-1/2 -translate-x-1/2 group-hover:bottom-10 duration-500 ease-in-out transform-gpu">
+                                        <h1 class="group text-center mt-2 mb-2 font-semibold text-lg text-primary  duration-1000 ease-in-out">
+                                            Hewan
+                                        </h1>
+                                        <p class="group hidden text-sm mb-3 group-hover:block  duration-1000 ease-in-out opacity-0 group-hover:opacity-100">
+                                            Jelajahi beragam hewan dari seluruh provinsi. Dari mamalia
+                                            eksotis hingga reptil yang misterius, yang telah kami
+                                            kumpulkan untuk kaamu nikmati
+                                        </p>
+                                        <Link href="/animals" class="group mx-auto hidden btn-primary-lite text-sm group-hover:block  duration-1000 ease-in-out opacity-0 group-hover:opacity-100">
+                                        Selengkapnya
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="w-full md:w-1/3 flex flex-col gap-2 mb-10">
+                            <div class="group">
+                                <div class="mt-10 z-30 bg-white h-[320px] relative p-5 rounded-xl shadow-sm w-full group-hover:h-[320px] group-hover:shadow-lg duration-500 ease-in-out">
+                                    <img src="/images/tari.webp" alt="animal" class="h-[70%] w-[90%] object-cover rounded-xl absolute top-5 left-1/2 -translate-x-1/2 group group-hover:-translate-y-1/2 group-hover:w-[250px] group-hover:h-[140px] duration-500 ease-in-out transform-gpu" />
+                                    <div class="group text-center w-full px-5 absolute bottom-5 left-1/2 -translate-x-1/2 group-hover:bottom-10 duration-500 ease-in-out transform-gpu">
+                                        <h1 class="group text-center mt-2 mb-2 font-semibold text-lg text-primary  duration-1000 ease-in-out">
+                                            Tari
+                                        </h1>
+                                        <p class="group hidden text-sm mb-3 group-hover:block  duration-1000 ease-in-out opacity-0 group-hover:opacity-100">
+                                            Selamat datang di dunia tari yang memukau. Menjelajahi
+                                            ragam gerakan, budaya, dan ekspresi yang terabadikan dalam
+                                            setiap tarian.
+                                        </p>
+                                        <Link href="/dances" class="group mx-auto hidden btn-primary-lite text-sm group-hover:block  duration-1000 ease-in-out opacity-0 group-hover:opacity-100">
+                                        Selengkapnya
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="group ">
+                                <div class="mt-5 z-30 bg-white h-[270px] relative p-5 rounded-xl shadow-sm w-full group-hover:h-[310px] group-hover:shadow-lg duration-500 ease-in-out">
+                                    <img src="/images/destination.webp" alt="animal" class="h-[70%] w-[90%] object-cover rounded-xl absolute top-5 left-1/2 -translate-x-1/2 group group-hover:-translate-y-1/2 group-hover:w-[250px] group-hover:h-[140px] duration-500 ease-in-out transform-gpu" />
+                                    <div class="group text-center w-full px-5 absolute bottom-5 left-1/2 -translate-x-1/2 group-hover:bottom-10 duration-500 ease-in-out transform-gpu">
+                                        <h1 class="group text-center mt-2 mb-2 font-semibold text-lg text-primary  duration-1000 ease-in-out">
+                                            Destinasi
+                                        </h1>
+                                        <p class="group hidden text-sm mb-3 group-hover:block  duration-1000 ease-in-out opacity-0 group-hover:opacity-100">
+                                            Tenemukan petualangan di setiap sudut dunia. Dari puncak
+                                            gunung yang menantang hingga pantai berpasir putih yang
+                                            menenangkan
+                                        </p>
+                                        <Link href="/destinations" class="group mx-auto hidden btn-primary-lite text-sm group-hover:block  duration-1000 ease-in-out opacity-0 group-hover:opacity-100">
+                                        Selengkapnya
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="group ">
+                                <div class="mt-5 z-30 bg-white h-[270px] relative p-5 rounded-xl shadow-sm w-full group-hover:h-[310px] group-hover:shadow-lg duration-500 ease-in-out">
+                                    <img src="/images/pakaian-adat.webp" alt="animal" class="h-[70%] w-[90%] object-cover rounded-xl absolute top-5 left-1/2 -translate-x-1/2 group group-hover:-translate-y-1/2 group-hover:w-[250px] group-hover:h-[140px] duration-500 ease-in-out transform-gpu" />
+                                    <div class="group text-center w-full px-5 absolute bottom-5 left-1/2 -translate-x-1/2 group-hover:bottom-10 duration-500 ease-in-out transform-gpu">
+                                        <h1 class="group text-center mt-2 mb-2 font-semibold text-lg text-primary  duration-1000 ease-in-out">
+                                            Pakaian Adat
+                                        </h1>
+                                        <p class="group hidden text-sm mb-3 group-hover:block  duration-1000 ease-in-out opacity-0 group-hover:opacity-100">
+                                            Tenemukan petualangan di setiap sudut dunia. Dari puncak
+                                            gunung yang menantang hingga pantai berpasir putih yang
+                                            menenangkan
+                                        </p>
+                                        <Link href="/costumes" class="group mx-auto hidden btn-primary-lite text-sm group-hover:block  duration-1000 ease-in-out opacity-0 group-hover:opacity-100">
+                                        Selengkapnya
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="w-full md:w-1/3 flex flex-col gap-2 mb-10">
+                            <div class="group">
+                                <div class="mt-10 z-30 bg-white h-[250px] relative p-5 rounded-xl shadow-sm w-full group-hover:h-[320px] group-hover:shadow-lg duration-500 ease-in-out">
+                                    <img src="/images/music.webp" alt="animal" class="h-[70%] w-[90%] object-cover rounded-xl absolute top-5 left-1/2 -translate-x-1/2 group group-hover:-translate-y-1/2 group-hover:w-[250px] group-hover:h-[140px] duration-500 ease-in-out transform-gpu" />
+                                    <div class="group text-center w-full px-5 absolute bottom-5 left-1/2 -translate-x-1/2 group-hover:bottom-10 duration-500 ease-in-out transform-gpu">
+                                        <h1 class="group text-center mt-2 mb-2 font-semibold text-lg text-primary  duration-1000 ease-in-out">
+                                            Musik
+                                        </h1>
+                                        <p class="group hidden text-sm mb-3 group-hover:block  duration-1000 ease-in-out opacity-0 group-hover:opacity-100">
+                                            Di sinilah harmoni tercipta. Menyelami beragam genre
+                                            musik, dari klasik hingga pop, dari rock hingga jazz
+                                        </p>
+                                        <Link href="/musics" class="group mx-auto hidden btn-primary-lite text-sm group-hover:block  duration-1000 ease-in-out opacity-0 group-hover:opacity-100">
+                                        Selengkapnya
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="group ">
+                                <div class="mt-5 z-30 bg-white h-[270px] relative p-5 rounded-xl shadow-sm w-full group-hover:h-[310px] group-hover:shadow-lg duration-500 ease-in-out">
+                                    <img src="/images/artefact.webp" alt="animal" class="h-[70%] w-[90%] object-cover rounded-xl absolute top-5 left-1/2 -translate-x-1/2 group group-hover:-translate-y-1/2 group-hover:w-[250px] group-hover:h-[140px] duration-500 ease-in-out transform-gpu" />
+                                    <div class="group text-center w-full px-5 absolute bottom-5 left-1/2 -translate-x-1/2 group-hover:bottom-10 duration-500 ease-in-out transform-gpu">
+                                        <h1 class="group text-center mt-2 mb-2 font-semibold text-lg text-primary  duration-1000 ease-in-out">
+                                            Artefak
+                                        </h1>
+                                        <p class="group hidden text-sm mb-3 group-hover:block  duration-1000 ease-in-out opacity-0 group-hover:opacity-100">
+                                            Merentasi waktu dengan melihat ke masa lalu melalui mata
+                                            artefak. Setiap benda adalah jendela ke sejarah yang
+                                            menghidupkan kembali cerita-cerita zaman dahulu.
+                                        </p>
+                                        <Link href="/artefacts" class="group mx-auto hidden btn-primary-lite text-sm group-hover:block  duration-1000 ease-in-out opacity-0 group-hover:opacity-100">
+                                        Selengkapnya
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="group ">
+                                <div class="mt-5 z-30 bg-white h-[270px] relative p-5 rounded-xl shadow-sm w-full group-hover:h-[310px] group-hover:shadow-lg duration-500 ease-in-out">
+                                    <img src="/images/gift.webp" alt="animal" class="h-[70%] w-[90%] object-cover rounded-xl absolute top-5 left-1/2 -translate-x-1/2 group group-hover:-translate-y-1/2 group-hover:w-[250px] group-hover:h-[140px] duration-500 ease-in-out transform-gpu" />
+                                    <div class="group text-center w-full px-5 absolute bottom-5 left-1/2 -translate-x-1/2 group-hover:bottom-10 duration-500 ease-in-out transform-gpu">
+                                        <h1 class="group text-center mt-2 mb-2 font-semibold text-lg text-primary  duration-1000 ease-in-out">
+                                            Hadiah
+                                        </h1>
+                                        <p class="group hidden text-sm mb-3 group-hover:block  duration-1000 ease-in-out opacity-0 group-hover:opacity-100">
+                                            Dapatkan Hadiah menarik dengan menyelesaikan soal
+                                        </p>
+                                        <Link href="/rewards" class="group mx-auto hidden btn-primary-lite text-sm group-hover:block  duration-1000 ease-in-out opacity-0 group-hover:opacity-100">
+                                        Selengkapnya
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
---
+++ templates/navbar.php
@@ -0,0 +1,61 @@
+<section class="font-poppins">
+        <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+            <span class="sr-only">Open sidebar</span>
+            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+            </svg>
+        </button>
+
+        <aside id="logo-sidebar" class="fixed top-0 left-0 z-10 w-72 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+            <div class="h-full flex flex-col justify-between p-12 overflow-y-auto bg-[#1A2035] text-white">
+                <div class="pl-4 ">
+                    <img src="../public/images/man.webp" class="w-[80px] h-[80px] object-cover rounded-xl" alt="" />
+                    <h1 class="font-boldf text-2xl mt-5">name</h1>
+                    <h4 class="sm:text-md text-gray-400">email</h4>
+                    <p class="text-sm text-yellow-400">100 Poin</p>
+                </div>
+                <ul class="space-y-2 font-medium text-lg">
+                    <li>
+                        <Link href="/home" class="flex items-center p-2 text-gray-100 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-gray-700 group">
+                        <span class="ml-3">Beranda</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/news" class="flex items-center p-2 text-gray-100 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-gray-700 group">
+                        <span class="flex-1 ml-3 whitespace-nowrap">Berita</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <a href="/about-us" class="flex items-center p-2 text-gray-100 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-gray-700 group">
+                            <span class="ml-3">Tentang Kami</span>
+                        </a>
+                    </li>
+                    <li>
+                        <Link href="/rewards" class="flex items-center p-2 text-gray-100 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-gray-700 group">
+                        <span class="flex-1 ml-3 whitespace-nowrap">
+                            Riwayat Hadiah
+                        </span>
+                        </Link>
+                    </li>
+                    <li class="-mb-10">
+                        <!-- <Link
+                href="/profiles"
+                class="${
+                  path === "/profiles"
+                    ? "mb-28 group active-menu"
+                    : "mb-28 flex items-center p-2 text-gray-100 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-gray-700 group"
+               
+              > -->
+                        <Link href="/profiles" class="mb-28 flex items-center p-2 text-gray-100 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-gray-700 group">
+                        <span class="flex-1 ml-3 whitespace-nowrap">
+                            Pengaturan Akun
+                        </span>
+                        </Link>
+                    </li>
+                </ul>
+                <div class="cursor-pointer flex items-center p-2 text-red-500 rounded-lg dark:text-white hover:bg-red-600 dark:hover:bg-gray-700 hover:text-gray-100 group" onClick={logout}>
+                    <span class="flex-1 ml-3 whitespace-nowrap">Keluar</span>
+                </div>
+            </div>
+        </aside>
+    </section>
