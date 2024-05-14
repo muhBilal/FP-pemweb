23f8b66e19b8e866eb948015b0010edc6acc72f2
muhBilal <mb336450@gmail.com>
2024-05-14 22:31:20 +0700

    fix: fix connection - change config get from env

--- config/connection.php
+++ config/connection.php
@@ -1,11 +1,24 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$databasename = "pemweb";
+$envFile = __DIR__ . '/../.env';
+
+$env = file_get_contents($envFile);
+$env = explode("\n", $env);
+$env = array_filter($env);
+$env = array_map(function ($item) {
+    return explode('=', $item);
+}, $env);
+$env = array_column($env, 1, 0);
+
+$servername = $env['DB_HOST'];
+$username = $env['DB_USER'];
+$password = $env['DB_PASS'];
+$databasename = $env['DB_NAME'];
+
 $conn = new mysqli($servername, $username, $password , $databasename);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+
 ?>
