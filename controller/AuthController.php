1e98242d06e8b64dee05dd55489f3a3c9c379179
Aswin-art <aswinarung2@gmail.com>
2024-05-26 00:20:22 +0700

    feat: add logout function

--- controller/AuthController.php
+++ controller/AuthController.php
@@ -5,78 +5,87 @@ require_once __DIR__ . '/../config/connection.php';
 class AuthController {
     public function login($data) {
         global $conn;
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $email = mysqli_real_escape_string($conn, $data['email']);
-            $password = mysqli_real_escape_string($conn, $data['password']);
+        $email = mysqli_real_escape_string($conn, $data['email']);
+        $password = htmlspecialchars($data['password']);
 
-            $query = "SELECT * FROM users WHERE email = '$email'";
-            $result = mysqli_query($conn, $query);
 
-            if ($result->num_rows > 0) {
-                $user = $result->fetch_assoc();
-                if (password_verify($password, $user['password'])) {
-                    $_SESSION['username'] = $user['username'];
-                    $_SESSION['email'] = $email;
-                    $_SESSION['role'] = $user['role'];
+        $query = "SELECT * FROM users WHERE email = '$email'";
+        $result = mysqli_query($conn, $query);
+    
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+            $verifyPassword = password_verify($password, $user['password']);
+        
+            if ($verifyPassword) {
+                $_SESSION['email'] = $email;
+                $_SESSION['username'] = $user['username'];
+                $_SESSION['role'] = $user['role'];
 
-                    if($user['role'] == 'visitor'){
-                        header("Location: pages/home.php");
-                    }else{
-                        header("Location: pages/admin/pakaian-adat/index.php");
-                    }
-                } else {
-                    $_SESSION['error'] = "Password salah!";
-                    header("Location: pages/auth/login.php");
+                if($user['role'] === 'visitor'){
+                    header("Location: ../../pages/home.php");
+                }else{
+                    header("Location: ../../pages/admin/pakaian-adat/index.php");
                 }
             } else {
-                $_SESSION['error'] = "User tidak ditemukan!";
-                header("Location: pages/auth/login.php");
-            }
+                $_SESSION['error'] = "Password salah!";
+                header("Location: ../../pages/auth/login.php");
 
-            $conn->close();
-            exit();
+            }
+        } else {
+            $_SESSION['error'] = "User tidak ditemukan!";
+            header("Location: ../../pages/auth/login.php");
         }
     }
 
     public function register($data) {
         global $conn;
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $username = mysqli_real_escape_string($conn, $data['username']);
-            $email = mysqli_real_escape_string($conn, $data['email']);
-            $password = mysqli_real_escape_string($conn, $data['password']);
-            $role = 'visitor'; // Default role
+        $username = mysqli_real_escape_string($conn, $data['username']);
+        $email = mysqli_real_escape_string($conn, $data['email']);
+        $password = mysqli_real_escape_string($conn, $data['password']);
+        $role = 'visitor'; // Default role
 
-            if (empty($username) || empty($email) || empty($password)) {
-                $_SESSION['error'] = "Semua bidang wajib diisi!";
-                header("Location: pages/auth/register.php");
-                exit();
-            }
-
-            $query = "SELECT * FROM users WHERE email = '$email'";
-            $result = mysqli_query($conn, $query);
-
-            if ($result->num_rows > 0) {
-                $_SESSION['error'] = "Email sudah terdaftar!";
-                header("Location: pages/auth/register.php");
-                exit();
-            }
-
-            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-            $query = "INSERT INTO users (username, email, password, role) VALUES ('$username', '$email', '$hashedPassword', '$role')";
-            if (mysqli_query($conn, $query)) {
-                $_SESSION['username'] = $username;
-                $_SESSION['email'] = $email;
-                $_SESSION['role'] = $role;
-                header("Location: pages/home.php");
-            } else {
-                $_SESSION['error'] = "Terjadi kesalahan saat mendaftar. Silakan coba lagi.";
-                header("Location: pages/auth/register.php");
-            }
-
-            $conn->close();
+        if (empty($username) || empty($email) || empty($password)) {
+            $_SESSION['error'] = "Semua bidang wajib diisi!";
+            header("Location: ../../pages/auth/register.php");
             exit();
         }
+
+        $query = "SELECT * FROM users WHERE email = '$email'";
+        $result = mysqli_query($conn, $query);
+
+        if ($result->num_rows > 0) {
+            $_SESSION['error'] = "Email sudah terdaftar!";
+            header("Location: ../../pages/auth/register.php");
+            exit();
+        }
+
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        $query = "INSERT INTO users (username, email, password, role) VALUES ('$username', '$email', '$hashedPassword', '$role')";
+        if (mysqli_query($conn, $query)) {
+            $_SESSION['username'] = $username;
+            $_SESSION['email'] = $email;
+            $_SESSION['role'] = $role;
+            header("Location: ../../pages/home.php");
+        } else {
+            $_SESSION['error'] = "Terjadi kesalahan saat mendaftar. Silakan coba lagi.";
+            header("Location: ../../pages/auth/login.php");
+        }
+
+        $conn->close();
+        exit();
+    }
+
+    public function logout(){
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $_SESSION = array();
+        session_destroy();
+
+        header("Location: ../../pages/auth/login.php");
+        exit();
     }
 }
 
