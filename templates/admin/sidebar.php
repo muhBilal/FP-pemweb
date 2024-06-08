355a8116cfe74cdce85f8b5e6875e754e7d52dc5
bilal <mb336450@gmail.com>
2024-06-08 19:31:36 +0700

    add active menu sidebar

--- templates/admin/sidebar.php
+++ templates/admin/sidebar.php
@@ -13,6 +13,8 @@ if (!file_exists($envFile)) {
 
     $appUrl = $env['APP_URL'];
 }
+
+$url = $_SERVER['REQUEST_URI'];
 ?>
 <!-- sidenav  -->
 <aside class="fixed inset-y-0 flex-wrap items-center justify-between block w-full p-0 my-4 overflow-y-auto antialiased transition-transform duration-200 -translate-x-full bg-white border-0 shadow-xl dark:shadow-none dark:bg-slate-850 max-w-64 ease-nav-brand z-990 xl:ml-6 rounded-2xl xl:left-0 xl:translate-x-0"
@@ -31,8 +33,8 @@ if (!file_exists($envFile)) {
     <div class="items-center block w-auto max-h-screen overflow-auto h-sidenav grow basis-full">
         <ul class="flex flex-col pl-0 mb-0">
             <li class="mt-0.5 w-full">
-                <a class="py-2.7 bg-blue-500/13 dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors"
-                   href="<?php echo $appUrl ?>/pages/admin/pakaian-adat/">
+                <a class="<?php echo ($url == '/pemweb/pages/admin/pakaian-adat/') ? 'py-2.7 bg-blue-500/13' : '' ?> py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
+                    href="<?php echo $appUrl ?>/pages/admin/pakaian-adat/">
                     <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                         <i class="relative top-0 text-sm leading-normal text-blue-500 ni ni-tv-2"></i>
                     </div>
@@ -49,10 +51,9 @@ if (!file_exists($envFile)) {
                     <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Makanan Daerah</span>
                 </a>
             </li>
-
             <li class="mt-0.5 w-full">
-                <a class=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
-                   href="http://localhost/fp-pemweb/pages/admin/tari-daerah/">
+                <a class="<?php echo ($url == '/pemweb/pages/admin/tari-daerah/') ? 'py-2.7 bg-blue-500/13' : '' ?> py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
+                   href="<?php echo $appUrl ?>/pages/admin/tari-daerah/">
                     <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center fill-current stroke-0 text-center xl:p-2.5">
                         <i class="relative top-0 text-sm leading-normal text-emerald-500 ni ni-credit-card"></i>
                     </div>
