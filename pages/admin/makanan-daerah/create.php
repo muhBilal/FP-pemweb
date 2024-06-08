e646330c5711eab6198ad13017f3041655791642
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-06-08 08:14:27 +0700

    feat: admin makanan-daerah

---
+++ pages/admin/makanan-daerah/create.php
@@ -0,0 +1,123 @@
+<?php
+require '../../../controller/makananDaerahController.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $errorMessages = [];
+    $uploadOk = 1;
+    $targetDir = "../../../public/images/upload/makanan-daerah/";
+
+    if ($_FILES["image"]["tmp_name"]) {
+        $imageFileType = strtolower(pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION));
+        $check = getimagesize($_FILES["image"]["tmp_name"]);
+        if ($check === false) {
+            $errorMessages[] = "File is not an image.";
+            $uploadOk = 0;
+        }
+        if ($_FILES["image"]["size"] > 5000000) {
+            $errorMessages[] = "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+        $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+        if (!in_array($imageFileType, $allowedExtensions)) {
+            $errorMessages[] = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+    }
+
+    if ($uploadOk == 1 && $_FILES["image"]["tmp_name"]) {
+        if (!is_dir($targetDir)) {
+            mkdir($targetDir, 0777, true);
+        }
+
+        $newFileName = uniqid() . '_' . time() . '.' . $imageFileType;
+        $targetFile = $targetDir . $newFileName;
+
+        if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
+            $imageUrl = $targetFile;
+            echo "The file " . htmlspecialchars(basename($_FILES["image"]["name"])) . " has been uploaded. URL: " . $imageUrl;
+
+            $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+            $description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_STRING);
+            $maps = filter_input(INPUT_POST, 'maps', FILTER_SANITIZE_URL);
+            $youtube = filter_input(INPUT_POST, 'youtube', FILTER_SANITIZE_URL);
+
+            $data = [
+                'name' => $name,
+                'description' => $description,
+                'maps' => $maps,
+                'youtube' => $youtube,
+                'image' => $imageUrl
+            ];
+
+            $result = insertMakananDaerah($data);
+
+            if ($result) {
+                header('Location: index.php');
+                exit();
+            } else {
+                echo "Insert failed.";
+            }
+
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+            $uploadOk = 0;
+        }
+    } else {
+        foreach ($errorMessages as $message) {
+            echo $message . "<br>";
+        }
+    }
+}
+?>
+
+<?php include '../../../templates/admin/head.php'; ?>
+<div class="flex flex-wrap m-[1rem]">
+    <div class="flex-none w-full max-w-full px-3">
+        <div class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
+            <div class="p-6 pb-0 mb-0 border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
+                <h6 class="text-2xl font-semibold dark:text-white mb-5">
+                    Tambah Data
+                </h6>
+
+                <form class="px-10 pb-10" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" enctype="multipart/form-data">
+                    <div class="mb-6">
+                        <label htmlFor="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            Nama
+                        </label>
+                        <input type="text" id="name" name="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nama" />
+                    </div>
+                    <div class="mb-6">
+                        <label htmlFor="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            Deskripsi
+                        </label>
+                        <input id="description" name="description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Deskripsi" />
+                    </div>
+                    <div class="mb-6">
+                        <label htmlFor="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            Maps
+                        </label>
+                        <input type="text" id="maps" name="maps" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Maps" />
+                    </div>
+                    <div class="mb-6">
+                        <label htmlFor="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            Youtube URL
+                        </label>
+                        <input type="text" id="youtube" name="youtube" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Youtube URL" />
+                    </div>
+                    <div class="mb-6">
+                        <label htmlFor="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            Image
+                        </label>
+                        <input type="file" id="image" name="image" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                    </div>
+                    <button class="px-4 py-3 bg-[#1A2035] hover:bg-gradient-to-br rounded-md text-white outline-none shadow-lg transform active:scale-x-75 transition-transform focus:ring-4 focus:ring-[#1A2035]" type="submit">
+                        <span class="font-2xl">Tambah Data</span>
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include '../../../templates/tail.php'; ?>
+
+
---
+++ pages/admin/makanan-daerah/index.php
@@ -0,0 +1,64 @@
+<?php
+require '../../../controller/makananDaerahController.php';
+$makanan_daerah = getMakananDaerah();
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    deleteMakananDaerah($id);
+    header('Location: index.php');
+}
+?>
+
+<?php include '../../../templates/admin/head.php'; ?>
+<!-- cards -->
+<div class="w-full px-6 py-6 mx-auto">
+    <div class="flex flex-wrap -mx-3">
+        <div class="flex-none w-full max-w-full px-3">
+            <div class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
+                <div class="flex justify-between items-center p-6">
+                    <div class="pb-0 mb-0 border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
+                        <h6 class="dark:text-white">Makanan Daerah</h6>
+                    </div>
+                    <a href="create.php" class="py-2 px-4 bg-transparent text-[#1A2035] font-semibold border border-[#1A2035] rounded hover:bg-[#1A2035] hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0">
+                        Tambah
+                    </a>
+                </div>
+                <div class="flex-auto px-0 pt-0 pb-2 overflow-x-auto">
+                    <div class="p-0">
+                        <table class="items-center w-full mb-0 align-top border-collapse dark:border-white/40 text-slate-500">
+                            <thead class="align-bottom">
+                                <tr>
+                                    <th class="px-6 py-3 font-bold text-left uppercase align-middle bg-transparent border-b border-collapse shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">Nama Makanan Daerah</th>
+                                    <th class="px-6 py-3 pl-2 font-bold text-left uppercase align-middle bg-transparent border-b border-collapse shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">Deskripsi</th>
+                                    <th class="px-6 py-3 font-bold text-center uppercase align-middle bg-transparent border-b border-collapse shadow-none dark:border-white/40 dark:text-white text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($makanan_daerah as $makanan) { ?>
+                                    <tr>
+                                        <td class="p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent">
+                                            <div class="flex px-2 py-1">
+                                                <div class="flex flex-col justify-center">
+                                                    <h6 class="mb-0 text-sm leading-normal dark:text-white"><?php echo $makanan['name']; ?></h6>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td class="p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent">
+                                            <p class="mb-0 text-xs font-semibold leading-tight dark:text-white dark:opacity-80"><?php echo substr($makanan['description'], 0, 200); ?>...</p>
+                                        </td>
+                                        <td class="p-2 align-middle bg-transparent border-b dark:border-white/40 whitespace-nowrap shadow-transparent">
+                                            <a href="/fp-pemweb/pages/admin/makanan-daerah/update.php?id=<?php echo $makanan['id']; ?>" class="text-xs font-semibold leading-tight dark:text-white dark:opacity-80 text-slate-400">Update</a>
+                                            <a href="index.php?id=<?php echo $makanan['id']; ?>" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')" class="text-xs font-semibold leading-tight dark:text-white dark:opacity-80 text-slate-400">Delete</a>
+                                        </td>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end cards -->
+<?php include '../../../templates/tail.php'; ?>
